aec07ff43460b1226c89ac241730fdd07aeed579
notanorange <grkim131@gmail.com>
2025-01-18 10:14:02 +0900

    반응형

--- pf/index.js
+++ pf/index.js
@@ -8,8 +8,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 // Projects 스와이퍼
 var projectSlide = new Swiper("#projects .mySwiper", {
-    slidesPerView: 3.5,
-    spaceBetween: 40,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
     loop: true,
     looppedAdditionalSlide: 1,
+    breakpoints: {
+        1024: {
+            slidesPerView: 3.5,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+        },
+        600: {
+            slidesPerView: 2.2,
+            spaceBetween: 20,
+        },
+    }
 });
